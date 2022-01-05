0841b7f2005d61fa2de84d800a210652dfe7db62
plaul <lmor@kea.dk>
2022-01-05 18:12:14 +0100

    cleanup

--- src/main/java/dk/plaul/demo1/Demo1Application.java
+++ src/main/java/dk/plaul/demo1/Demo1Application.java
@@ -1,13 +1,24 @@
 package dk.plaul.demo1;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
-public class Demo1Application {
+public class Demo1Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
+	}
+
+	@Autowired
+	private DataSource dataSource;
+	@Override
+	public void run(String...args) throws Exception {
+		System.out.println("DATASOURCE = " + dataSource);
 	}
 
 }
